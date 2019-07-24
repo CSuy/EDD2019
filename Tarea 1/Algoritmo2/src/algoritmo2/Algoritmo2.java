4363b43abf4bb4986a3d7921f980d08329334d54
CSuy <53056343+CSuy@users.noreply.github.com>
2019-07-23 19:58:55 -0600

    Add files via upload

---
+++ Tarea 1/Algoritmo2/src/algoritmo2/Algoritmo2.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package algoritmo2;
+
+/**
+ *
+ * @author MiriamLeticia
+ */
+public class Algoritmo2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Ventana1 c = new Ventana1();
+        c.setVisible(true);
+    }
+    
+}
